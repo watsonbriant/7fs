4938920ff44e1d09285627be2040651651e7e3eb
Brian Watson <dripfield.pro@gmail.com>
2026-06-06 21:57:55 -0400

    0.0.1 - Update Next.js configuration and layout; add rewrites for root path, change site metadata, and remove default home page.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/index.html",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
